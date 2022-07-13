865ba55b2c77487bc9d68de09cc8ee749625320b
leeal <leealexandra58@gmail.com>
2022-07-13 18:05:44 -0400

    without extended challenge

---
+++ script.js
@@ -0,0 +1,42 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+
+let v = 0
+while (v < 10) {
+    v++;
+    console.log(v);
+}
+
+
+let a = 11
+while (a > 1) {
+    a--;
+    console.log(a);
+}
+
+
+let k = 1
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
+
+
+let i = 10
+do {
+      console.log(i);
+      i--;
+} while (i >= 1);
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+for (let j = 0; j < numbers.length; j++) {
+    console.log(numbers[j]);
+}
